cb63a7a3e515fcd5fcda6a0267be7ae5cb623269
EnzoPonce <enzoponce21@gmail.com>
2019-10-29 20:57:08 -0300

    Enzo

---
+++ interfaz_sudoku.py
@@ -0,0 +1,57 @@
+from sudoku_game import Sudoku
+
+
+class interfaz():
+
+    def validar(self, fila, columna, numero):
+        try:
+            if int(fila) > 0 and int(fila) < 9:
+                return True
+            if int(columna) > 0 and int(columna) < 9:
+                return True
+            if numero != "x":
+                if int(numero) > 0 and int(columna) < 9:
+                    return True
+        except:
+            return False
+
+    def __init__(self):
+        self.table = [["5", "3", "x", "x", "7", "x", "x", "x", "x"],
+                      ["6", "x", "x", "1", "9", "5", "x", "x", "x"],
+                      ["x", "9", "8", "x", "x", "x", "x", "6", "x"],
+                      ["8", "x", "x", "x", "6", "x", "x", "x", "3"],
+                      ["4", "x", "x", "8", "x", "3", "x", "x", "1"],
+                      ["7", "x", "x", "x", "2", "x", "x", "x", "6"],
+                      ["x", "6", "x", "x", "x", "x", "2", "8", "x"],
+                      ["x", "x", "x", "4", "1", "9", "x", "x", "5"],
+                      ["x", "x", "x", "x", "8", "x", "x", "7", "9"]]
+        self.game = Sudoku(self.table)
+
+    def PrintTable(self):
+        table = ""
+        for fila in self.table:
+            for elemento in fila:
+                table += elemento + " "
+            table += "\n"
+        return table
+
+    def jugar(self):
+        print(self.PrintTable())
+        while not self.game.gano():
+            self.n = input("ingrese un numero ")
+            self.i = input("ingrese la fila ")
+            self.j = input("ingrese la columna ")
+
+            if self.validar(self.i, self.j, self.n):
+                self.i = int(self.i)
+                self.j = int(self.j)
+                if self.game.validar(self.i, self.j, self.n) and self.game.checkeo_zona(self.i, self.j, self.n):
+                    if not (self.i, self.j) in self.game.tablero_original:
+                        self.game.intercambio_numero(self.i, self.j, self.n)
+
+            print(self.PrintTable())
+
+
+x = interfaz()
+
+x.jugar()
---
+++ sudoku_game.py
@@ -0,0 +1,60 @@
+import math
+
+class Sudoku():
+
+    def __init__(self, lista):
+        self.table = lista
+
+        self.tablero_original = []
+        for i in range(len(self.table)):
+            for j in range(len(self.table)):
+                if (self.table[i][j] != "x"):
+                    self.tablero_original.append((i, j))
+
+    def validar(self, fila, columna, numero):
+        #verifica si se repite algun numero en alguna fila
+        for elemento in self.table[fila]:
+            if elemento == numero:
+                return False
+            else:
+                return True
+        #verifica si se repite algun numero en la columna
+        for elemento in range(9):
+            if self.table[elemento][columna] == numero:
+                return False
+            else:
+                True
+        
+        #verifica si se repite algun numero en alguna zona
+
+    def checkeo_zona(self, fila, columna, valor):
+        fila = (fila // 3) * 3
+        columna = (columna // 3) * 3
+        for i in range(3):
+            for j in range(3):
+                if (self.table[int(fila + i)][int(columna + j)] == str(valor)):
+                    return False
+        return True
+
+    
+    def intercambio_numero(self,fila,columna,valor):
+
+        if self.checkeo_zona(fila,columna,valor) and self.validar(fila,columna,valor):
+            self.table [fila][columna] = valor
+            return True
+        else:
+            return False
+
+    def gano(self):
+        for fila in self.table:
+            if "x" in fila:
+                return False
+            else:
+                return True
+
+    
+
+
+    
+
+
